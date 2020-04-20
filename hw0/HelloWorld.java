8452f57ac913ea945c99cba8adc38a6776641d07
darktef <darktef@gmail.com>
2020-04-19 21:55:44 -0400

    hw0

---
+++ hw0/HelloWorld.java
@@ -0,0 +1,10 @@
+/**
+ * This is trial class to print hello world message
+ */
+public class HelloWorld
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello World!");
+    }
+}
